3ffa9e72c73143764e77a105c80d234630ce8f18
adityasingh-ops <adi7753071602@gmail.com>
2026-03-02 17:44:11 +0530

    Update app navigation and layout
    - Modified tab layout to integrate custom components
    - Updated index screen to use new screen list component
    - Updated root layout for app configuration
    - Removed default explore and modal screens

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,35 +1,26 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
-
-import { HapticTab } from '@/components/haptic-tab';
-import { IconSymbol } from '@/components/ui/icon-symbol';
-import { Colors } from '@/constants/theme';
-import { useColorScheme } from '@/hooks/use-color-scheme';
+import React from 'react'
+import { Tabs } from 'expo-router'
+import MaterialIcons from '@expo/vector-icons/MaterialIcons'
+import AppHeader from '@/components/common/header'
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
   return (
     <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-      }}>
-      <Tabs.Screen
-        name="index"
+    >
+        <Tabs.Screen
+        name='index'
         options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+            title:'home',
+            tabBarIcon:({color}) => <MaterialIcons name='home' size={28} color={color}/>,
+            header:()=><AppHeader title={"Home"}/>
         }}
-      />
-      <Tabs.Screen
-        name="explore"
+        />
+        <Tabs.Screen
+        name='examples'
         options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+            tabBarIcon: ({color}) => <MaterialIcons name='book' size={28} color={color}/>
         }}
-      />
+        />
     </Tabs>
-  );
+  )
 }
